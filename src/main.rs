64324bd2edc7558083f3515aacf53781e87f9987
Suthiphong T <suthiphong@devcrew.co.th>
2024-04-15 00:42:32 +0700

    Update user, auth controller

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,8 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .route("/health-check", actix_web::web::get().to(routes::health_check::health_check))
-            .service(
-                web::scope("/user")
-                    .configure(routes::user_handler::user_handler::config)
-            )
-            .service(
-                web::scope("/auth")
-                    .configure(routes::auth_handler::auth_handler::config)
-            )
+            .configure(routes::user_handler::user_handler::config)
+            .configure(routes::auth_handler::auth_handler::config)
     })
     .bind(("0.0.0.0", 8080))?
     .run()
